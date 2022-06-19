75026239d5b267fc5318e27415972e58b264e821
Bharath <bharathchandra630@gmail.com>
2022-06-19 12:29:04 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+import { Recipe } from "src/app/recipes/recipe.model";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  loadedFeature = "recipe";
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    console.log("recipe is", recipe);
+  }
+}
